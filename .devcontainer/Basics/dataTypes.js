3214eecfd14ec77c66437ae0030b72165cbf0fb1
Rishabh6117 <rishikhaswin@gmail.com>
2026-09-10 07:51:20 +0000

    data Type

---
+++ .devcontainer/Basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use Strict" ; // treat all js code as newer version 
+
+// alert (3 + 3) we are using nodejs not browser 
+
+let name = "Rishabh" //string
+let age = 20 //number
+let isLoggedIn = true //boolean
+
+// premitive data type 
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true and false
+// null => standalone value
+// undefined => declared value but value is not assigned yet 
+// symbol => Uniqueness
+
+
+// object
+
+console.log(typeof null);
---
+++ .devcontainer/Basics/variable.js
@@ -0,0 +1,23 @@
+const accountId = 517732
+
+let accountEmail = "ranga3@yopmail.com"
+var accountPassword = "123"
+var accountPassword = "234"
+
+// Prefer not to use Var 
+// because giving issue with functional scope and block scope
+
+accountCity = "Punjab"
+
+let accountState;
+
+// accountId = 3
+
+accountEmail = "hjdgjf.com"
+accountPassword = "33"
+accountCity = "Chandigarh"
+
+console.log(accountId);
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
