b1e91e07c4d64d88cb333a637a934c0fc7690952
Rishabh6117 <rishikhaswin@gmail.com>
2026-09-10 08:31:41 +0000

    conversion comparision

---
+++ .devcontainer/Basics/comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+
+// console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+
+// // The reason is that an equality check == and comparisom > <>= <= work differently .
+//  Comparision convert null to a number, treating it as 0. 
+// That's why (3) null and (1) null >0 is false.
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0); 
---
+++ .devcontainer/Basics/conversionOprations.js
@@ -0,0 +1,60 @@
+// let score = 77 ;
+// let score = null ;
+// let score = undefined ;
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// "77" => 77
+// "88abc" => NaN
+// true => 1 ; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 44;
+
+let stringNumber = String(someNumber)
+// console.log(someNumber);
+// console.log(typeof someNumber);
+
+
+// ******************** Oprations ***********************
+
+let value = 10 ;
+
+let nagValue = -value
+// console.log((nagValue));
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "What"
+let str2 = "is this"
+
+let str3 = str1 = str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) *5 %3);
+
+// console.log(true);
+// console.log(+ "");
+
+
+
